f0f76c94c14a3592c13b3b2d0f75f6870ac9a74e
eyal segal <eyal.segal@aidoc.com>
2019-07-21 17:34:25 +0300

    learnt classes

--- training2.py
+++ training2.py
@@ -4,4 +4,20 @@ class Student:
         self.school = school
         self.marks = []
 
-    def average
+    def average(self):
+        return sum(self.marks)/len(self.marks)
+
+    @classmethod
+    def friend(cls, origin, friend_name):
+        return cls(friend_name, origin.school)
+
+
+class WorkingStudent(Student):
+    def __init__(self, name, school, salary):
+        super().__init__(name, school)
+        self.salary = salary
+
+
+anna = WorkingStudent("anna", "Oxford", 1500)
+friend = WorkingStudent.friend(anna, "Greg")
+print(friend.school)
